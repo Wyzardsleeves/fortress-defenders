358ece6279c41d1ee7cb123099fd47fc84c22a21
Justin L. Lattimore <jlattimor3@gmail.com>
2018-06-27 00:40:04 -0400

    Get, Post, Delete are functioning properly. Usable

--- client/src/components/cards/cards.js
+++ client/src/components/cards/cards.js
@@ -6,17 +6,18 @@ class Cards extends Component {
     super();
     this.state = {
       cards: [],
-      index: 0
     }
 
-    this.addCard = this.addCard.bind(this);
-    this.removeCard = this.removeCard.bind(this);
     this.getCards = this.getCards.bind(this);
+    this.addCard = this.addCard.bind(this);
+    this.updateCard = this.updateCard.bind(this);
+    this.removeCard = this.removeCard.bind(this);
   }
 
-  componentDidMount(){
+  componentWillMount(){
     this.getCards();
-    this.addCard();
+    this.addCard;
+    this.removeCard;
   }
 
   //gets cards via fetch
@@ -27,11 +28,12 @@ class Cards extends Component {
   }
 
   //add card via post request
-  addCard(){
-    console.log(this.refs.cardName.value);
-    console.log(this.refs.cardType.value);
-    let addName = String(this.refs.cardName.value);
-    let addType = String(this.refs.cardType.value);
+  addCard(e){
+    e.preventDefault();
+    let addName = this.refs.cardName.value;
+    let addType = this.refs.cardType.value;
+    console.log(addName);
+    console.log(addType);
     fetch('/api/cards/', {
       method: 'POST',
       body: JSON.stringify(
@@ -43,7 +45,10 @@ class Cards extends Component {
       headers: {
         'Content-Type': 'application/json'
       }
-    }).then(res => res.json());
+    })
+    .then(res => res.json());
+    console.log("Just added " + addName + " as a " + addType + " card!");
+    this.getCards();  //refreshes list
   }
 
   //edit a card via put request
@@ -64,27 +69,45 @@ class Cards extends Component {
     fetch('/api/cards/' + id, {
       method: 'DELETE'
     })
-    //.then(res => res.json());
+    .then(res => res.json())
+    .then(console.log(card.name + ' deleted!'));
+    this.getCards();  //refreshes list
   }
 
   render() {
     return (
       <div className="cards">
         <h2>Cards Working!</h2>
-        <form onSubmit={() => this.addCard}>
+        <form onSubmit={this.addCard}>
           <input ref="cardName" placeholder="Name here" type="text" name="cardName" />
           <select ref="cardType">
-            <option value="blue">Blue</option>
-            <option value="red">Red</option>
-            <option value="green">Green</option>
-            <option value="yellow">Yellow</option>
+            <option value="cleric">Cleric</option>
+            <option value="mage">Mage</option>
+            <option value="warrior">Warrior</option>
+            <option value="marksman">Marksman</option>
+            <option value="trickster">Trickster</option>
+            <option value="beast">Beast</option>
+            <option value="spirit">Spirit</option>
+            <option value="black_smith">Black Smith</option>
+            <option value="siege">Siege</option>
+            <option value="action">Action</option>
+            <option value="reaction">Reaction</option>
+            <option value="fortress">Fortress</option>
+            <option value="territory">Territory</option>
+          </select>
+          <select ref="cardFaction">
+            <option value="##462c63">Bedlamal</option> {/* Indigo */}
+            <option value="#48a548">Forest</option> {/* Green */}
+            <option value="#a33030">Savage Lands</option> {/* Crimson */}
+            <option value="#fbffc9">Aubadel Orda</option> {/* Cream */}
+            <option value="#e2e2e2">Neutral</option> {/* Silver */}
           </select>
           <input type="submit" value="Submit" />
         </form>
         <ul>
           {this.state.cards.map((card) =>
             <li key={card._id}>
-              {card.name} - {card.type} <span onClick={(e) => this.removeCard(e, card)}>X</span>
+              {card.name} - {card.type} <span className="close" onClick={(e) => this.removeCard(e, card)}>X</span>
             </li>
           )}
         </ul>
