5c66fcf3a553d12b6fff8cf5b627fe2e5b3220c1
Justin L. Lattimore <jlattimor3@gmail.com>
2018-07-08 05:38:19 -0400

    added update

--- client/src/components/cards/cards.js
+++ client/src/components/cards/cards.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import './cards.css';
 
+const cardsUrl= '/api/cards/';  //url for cards
+
 class Cards extends Component {
+
   constructor(){
     super();
     this.state = {
@@ -16,6 +19,7 @@ class Cards extends Component {
 
   componentWillMount(){
     this.getCards();
+    this.updateCard;
     this.addCard;
     this.removeCard;
   }
@@ -23,7 +27,7 @@ class Cards extends Component {
   //function for splicing text
   infoCleanup(info){
     if(info.length > 50){
-      return info.slice(0, 80) + "... ";
+      return info.slice(0, 60) + "... ";
     }
     else if(info.length < 50){
       let num = 50 - info.length;
@@ -37,9 +41,16 @@ class Cards extends Component {
 
   //gets cards via fetch
   getCards(){
-    fetch('/api/cards')
+    fetch(cardsUrl)
       .then(res => res.json())
       .then(cards => this.setState({cards}, () => console.log('Cards fetched..', cards)));
+  }
+
+  //sort cards
+  sortCards(cards){
+    this.cards.sort(function(a, b){
+      return a.value - b.value;
+    })
   }
 
   //add card via post request
@@ -79,10 +90,7 @@ class Cards extends Component {
           return("#e2e2e2");
       }
     };
-    console.log(addName);
-    console.log(addType);
-    console.log(addFaction);
-    fetch('/api/cards/', {
+    fetch(cardsUrl, {
       method: 'POST',
       body: JSON.stringify(
         {
@@ -113,21 +121,77 @@ class Cards extends Component {
   }
 
   //edit a card via put request
-  updateCard(e){
-    console.log(e);
-    fetch('/api/card/' + e._id, {
-      method: 'DELETE',
+  updateCard(e, card){
+    e.preventDefault();
+    let id = card._id;
+    let editName = this.refs.cardName.value;
+    let editType = this.refs.cardType.value;
+    let editFaction = this.refs.cardFaction.value;
+    let editHp = this.refs.cardHp.value;
+    let editDef = this.refs.cardDef.value;
+    let editBaseAp = this.refs.cardBaseAp.value;
+    let editRank = this.refs.cardRank.value;
+    let editReq = this.refs.cardReq.value;
+    let editImg = this.refs.cardImgUrl.value;
+    let editPassive = this.refs.cardPassive.value;
+    let editSkill1 = this.refs.cardSkill_1.value;
+    let editSkill2 = this.refs.cardSkill_2.value;
+    let editSkill3 = this.refs.cardSkill_3.value;
+    let editColor = function(){
+      switch(editFaction){
+        case "Bedlamal":
+          return("#a971e8")
+          break;
+        case "Savage Lands":
+          return("#a33030")
+          break;
+        case "Forest":
+          return("#48a548")
+          break;
+        case "Aubadel Orda":
+          return("#fbffc9")
+          break;
+        case "Neutral":
+          return("#e2e2e2")
+          break;
+        default:
+          return("#e2e2e2");
+      }
+    };
+    fetch(cardsUrl + id, {
+      method: 'PUT',
+      body: JSON.stringify(
+        {
+          "name": editName,
+          "type": editType,
+          "faction": editFaction,
+          "color": editColor(),
+          "hp": editHp,
+          "def": editDef,
+          "base_ap": editBaseAp,
+          "rank": editRank,
+          "req": editReq,
+          "image_url": editImg,
+          "passive": editPassive,
+          "skill_1": editSkill1,
+          "skill_2": editSkill2,
+          "skill_3": editSkill3
+        })
+      ,
       headers: {
         'Content-Type': 'application/json'
       }
     })
+    .then(res => res.json())
+    .then(console.log(card.name + ' updated!'));
+    this.getCards();  //refreshes list
   }
 
   //deleteCard
   removeCard(e, card){
     e.preventDefault();
     let id = card._id;
-    fetch('/api/cards/' + id, {
+    fetch(cardsUrl + id, {
       method: 'DELETE'
     })
     .then(res => res.json())
@@ -142,20 +206,23 @@ class Cards extends Component {
         <div className="card-title">
           <h2>Cards Working!</h2>
         </div>
+        {/* Going to stick a modal here for the updates */}
+
+        {/* Going to stick a modal here for the updates */}
         <div className="legend">
           <i style={{color: "#a971e8"}} className="ion ion-record">Bedlamal</i>
           <i style={{color: "#48a548"}} className="ion ion-record">Forest</i>
           <i style={{color: "#a33030"}} className="ion ion-record">Savage Lands</i>
           <i style={{color: "#fbffc9"}} className="ion ion-record">Aubadel Orda</i>
           <i style={{color: "#e2e2e2"}} className="ion ion-record">Neutral</i>
-        </div>        
+        </div>
         <div className="new-list">
           <ul>
             <li>
               <div style={{backgroundColor: "#bef4c2"}} className="whole-card">
-                <div>New Card</div>
+                <div style={{color: "green"}}>New Card</div>
                 <div className="card-input">
-                  <form id="submit-form" onSubmit={this.addCard}>
+                  <form id="submit-form">
                     <input ref="cardName" placeholder="Name here" type="text" name="cardName" />
                     <input ref="cardHp" placeholder="HP Amount" type="text" name="cardHp" />
                     <input ref="cardDef" placeholder="Defense here" type="text" name="cardDef" />
@@ -184,19 +251,18 @@ class Cards extends Component {
                       <option value="territory">Territory</option>
                     </select>
                     <select ref="cardFaction">
+                      <option value="-">-</option>
                       <option value="Bedlamal">Bedlamal</option> {/* Indigo #a971e8 */}
                       <option value="Forest">Forest</option> {/* Green #48a548 */}
                       <option value="Savage Lands">Savage Lands</option> {/* Crimson #a33030 */}
                       <option value="Aubadel Orda">Aubadel Orda</option> {/* Cream #fbffc9 */}
                       <option value="Neutral">Neutral</option> {/* Silver #e2e2e2 */}
                     </select>
-                    <input type="submit" value="Submit" />
                   </form>
                 </div>
               </div>
               <div className="card-foot">
-                {/* <i className="ion-close update" onClick={(e) => this.updateCard(e, card)}></i> */}
-                <i className="ion-plus-round new"></i><br/>
+                <i className="ion-plus-round new" onClick={this.addCard}></i><br/>
               </div>
             </li>
             {this.state.cards.reverse().map((card) =>
@@ -207,7 +273,7 @@ class Cards extends Component {
                       {card.name}
                     </div>
                     <div className="right">
-                      HP: {card.hp}
+                      <span style={{color:"#3c7ee8"}}>HP:</span> {card.hp}
                     </div>
                   </div>
                   <div className="full">
@@ -217,10 +283,10 @@ class Cards extends Component {
                   </div>
                   <div className="full">
                     <div className="underline left">
-                      Rank: {card.rank}
+                      {card.rank}
                     </div>
                     <div className="underline right">
-                      Req: {card.req}
+                      {card.req}
                     </div>
                   </div>
                   <div className="underline justified full">
@@ -249,26 +315,13 @@ class Cards extends Component {
                   </div>
                 </div>
                 <div className="card-foot">
-                  {/* <i className="ion-close update" onClick={(e) => this.updateCard(e, card)}></i> */}
-                  <i className="ion-edit update"></i>
-                  <i className="ion-close close" onClick={(e) => this.removeCard(e, card)}></i><br/>
+                  <i className="ion-edit update" onClick={(e) => this.updateCard(e, card)}></i>
+                  <i className="ion-close close" onClick={(e) => this.removeCard(e, card)}></i>
                 </div>
               </li>
             )}
           </ul>
         </div>
-        {/* this is the original
-        <div className="card-list">
-          <ul>
-            {this.state.cards.map((card) =>
-              <li onChange={this.getCards} key={card._id}>
-                {card.name} - {card.type} - {card.faction} - {card.color} <span className="close" onClick={(e) => this.removeCard(e, card)}>X</span><br/>
-                <img width="100%" src={card.image_url} />
-              </li>
-            )}
-          </ul>
-        </div>
-        */}
       </div>
     );
   }
