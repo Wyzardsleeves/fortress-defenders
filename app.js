c78c18f83594edc4a9716a225b01055f7c328826
Justin L. Lattimore <jlattimor3@gmail.com>
2018-07-17 04:06:09 -0400

    process name change

--- app.js
+++ app.js
@@ -22,13 +22,13 @@ app.get('/', function(req, res){
 */
 /* ------------------------ Phase out for development--------------------------- */
 //server static assets if in production
-//if(proccess.env.NODE_ENV === 'production'){
+if(process.env.NODE_ENV === 'production'){
   //setstatic folder
   app.use(express.static('client/build'));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
-//}
+}
 /* ------------------------ Phase out for development--------------------------- */
 
 //fetch types
